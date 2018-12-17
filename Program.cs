493b7465adf0cf0182c53c4e2d033f701a083a98
MickaelHm <harmeau.mickael@gmail.com>
2018-12-17 17:12:37 +0100

    Exercice Complet

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Triangle ABC rectangle en C
+            double AB;
+            int BC;
+            int CA;
+
+            Console.WriteLine("Donnez la longueur de BC");
+            BC = int.Parse(Console.ReadLine());
+            Console.WriteLine("Donnez la longueur de CA");
+            CA = int.Parse(Console.ReadLine());
+
+            AB = Math.Sqrt(Math.Pow(BC, 2) + Math.Pow(CA, 2));
+
+            Console.WriteLine("La longueur de AB est donc de " + AB);
+
+
+        }
+    }
+}
